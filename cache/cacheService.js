3b7744f55c5d5e2e4a57ece8132050d14fdf404c
Lahiru Sandaruwan <lahirus250@gmail.com>
2025-01-26 01:38:00 +0530

    refresh token caching layer was implmented using redis "Set" data type

--- cache/cacheService.js
+++ cache/cacheService.js
@@ -1,35 +1,46 @@
 import { createClient } from "redis";
 
+const refreshTokensSetName = "refreshTokens";
+
 const client = await createClient()
   .on("error", (err) => console.log("Redis client error", err))
   .connect();
 
-async function saveToken(username, token) {
+/**
+ * save token
+ * @param {*} token
+ */
+async function saveToken(token) {
   try {
-    await client.set(username, token);
+    await client.sAdd(refreshTokensSetName, token);
   } catch (error) {
-    console.log("saveTokenInRedisCache is failed. Error:" + error);
+    console.error("saveTokenInRedisCache is failed. Error:" + error);
   }
 }
 
-async function isTokenValid(username, token) {
+/**
+ * delete token
+ * @param {*} username
+ */
+async function removeToken(token) {
   try {
-    const storedToken = await client.get(username);
-    if (storedToken && token === storedToken) {
-      return true;
-    }
-    return false;
+    await client.sRem(refreshTokensSetName, token);
   } catch (error) {
-    console.log("isTokenInRedisCache is failed. Error: " + error);
+    console.error("saveTokenInRedisCache is failed. Error:" + error);
   }
 }
 
-// async function isTokenDeleted(username) {
-//   try {
-//     const a = await client.del()
-//   } catch (error) {
-    
-//   }
-// }
+/**
+ * check token validity
+ * @param {*} token
+ * @returns
+ */
+async function isTokenValid(token) {
+  try {
+    return await client.sIsMember(refreshTokensSetName, token);
+  } catch (error) {
+    console.error("isTokenValid is failed. Error: " + error);
+  }
+}
 
-export { saveToken, isTokenValid };
+export { saveToken, isTokenValid, removeToken };
